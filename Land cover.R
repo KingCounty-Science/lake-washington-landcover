92b4c50263ccefc325102dfa66bcf705e9936b06
Daniel Nidzgorski <daniel.nidzgorski@kingcounty.gov>
2024-03-13 13:21:34 -0700

    Read in and futz with coordinates.

--- Land cover.R
+++ Land cover.R
@@ -4,7 +4,20 @@
 
 library(tidyverse)
 library(sf)
+library(terra)
 library(tictoc)
 
 rm(list=ls())
 
+# Lake Washington watershed
+watershed<-st_read("Watershed/LakeWashingtonWatershed.shp") %>% 
+  st_transform(crs="ESRI:102008")
+
+
+# Initial test with NLCD 2021
+nlcd<-rast("NLCD/nlcd_2021_land_cover_l48_20230630.img") 
+crs(nlcd)<-"ESRI:102008"
+
+# Crop and mask to watershed extent
+n<-crop(nlcd,watershed,mask=T)
+writeRaster(n,"test.tif", overwrite=TRUE)
